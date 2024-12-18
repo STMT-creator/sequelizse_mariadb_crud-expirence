8505f8266b5138c728cc65e63827b56e6c8d62e4
STMT <elahep113@naver.com>
2024-12-18 16:04:09 +0900

    시퀄라이즈 DB 연결 설정

---
+++ models/index.js
@@ -0,0 +1,20 @@
+// const Sequelize = require('sequelize');
+import Sequelize from 'sequelize';
+// const config = require(__dirname + '/../config/config.json')[env];
+import config from '../config/config.json' assert { type: 'json' };
+const env = process.env.NODE_ENV || 'development';
+const nowConfig = config[env];
+
+const db = {};
+
+const sequelize = new Sequelize(
+  nowConfig.database,
+  nowConfig.username,
+  nowConfig.password,
+  nowConfig,
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
