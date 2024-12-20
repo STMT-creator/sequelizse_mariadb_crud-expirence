552cd6eb5dba6b04c3996b46a3ef6ff30032b8ac
STMT <elahep113@naver.com>
2024-12-20 16:07:31 +0900

    view engine | app set configure

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
+import nunjucks from 'nunjucks';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.js';
 import db from './models/index.js';
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+});
 
 // Model Synchronization : 모델-DB 동기화
 db.sequelize.sync({force:false})
 
-const app = express();
-const port = process.env.PORT || 3000;
-
+app.use(express.static('public')); // 미들웨어들 먼저 불러 오고 실행이 되어야해서 미들웨어가 먼저 온다
 app.use(express.json()); // json 파싱
 app.use(express.urlencoded({ extended: true })); // form 데이터 파싱
-app.use(express.static('public')); // 미들웨어들 먼저 불러 오고 실행이 되어야해서 미들웨어가 먼저 온다
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
