18bd74cc96255a0e5002b504a5c80ef94114b4bd
STMT <elahep113@naver.com>
2024-12-20 16:09:40 +0900

    user | user model 컨트롤러 분리

---
+++ controller/user.js
@@ -0,0 +1,21 @@
+import User from '../models/User.js';
+/**
+ * CRUD 작명을 잘해야한다.
+ * C = Create => addUser
+ * R = Read => getUser
+ * U = Update => editUser
+ * D = Delete => delUser
+ */
+
+const addUser = async (req, res) => {
+  try {
+    const { name, age, married } = req.body;
+    const user = await User.create({ name, age, married });
+    console.log(name, age, married);
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default { addUser };
