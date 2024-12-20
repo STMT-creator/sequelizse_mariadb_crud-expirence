e395e0c9eb0aceffd5080b1d3d285dcc653c9b51
STMT <elahep113@naver.com>
2024-12-20 16:10:59 +0900

    index | 라우터 컨트롤러 분리

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
+import indexController from '../controller/index.js';
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.send('새로운 세상을 만들어가');
-});
+router.route('/').get(indexController.getHome);
 
 export default router;
