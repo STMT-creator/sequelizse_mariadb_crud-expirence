2ad96904b29e737f6fe11f96594633b33cf041b2
STMT <elahep113@naver.com>
2024-12-23 16:09:58 +0900

    controller | 유저 모델 , try catch

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,19 @@
-const getHome = (req, res) => {
-  //   res.send('안녕하세요!');
-  res.render('index.html', {
-    title: '웹개발 실습',
-    subtitle: '사용자 등록 폼'
-});
+import User from "../models/User.js";
+
+const getHome = async (req, res) => {
+  try {
+    // 현재 등록된 유저 데이터 수신
+    const users = await User.findAll({
+      order: [['id', 'DESC']]
+    });
+    res.render('index.html', {
+      title: '웹개발 실습',
+      subtitle: '사용자 등록 폼',
+      users,
+  });
+  } catch (err) {
+    console.log(err)
+  }
 };
 
 export default { getHome };
