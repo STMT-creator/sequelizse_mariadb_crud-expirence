2fba144a5d2c10268a83ac0ffbd931f316696959
STMT <elahep113@naver.com>
2024-12-23 16:12:21 +0900

    get users, fetch => axios api

--- public/main.js
+++ public/main.js
@@ -4,24 +4,31 @@
  * name, age, married
  */
 
-const regiForm = document.querySelector("#regi-form");
-regiForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const name = e.target.username.value;
-    const age = e.target.age.value;
-    const married = e.target.married.checked;
+const regiForm = document.querySelector('#regi-form');
+regiForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const name = e.target.username.value;
+  const age = e.target.age.value;
+  const married = e.target.married.checked;
 
-    const response = await fetch("/users", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name,
-            age,
-            married
-        }),
-    });
-    const data = await response.json();
-    console.log(data)
+  const response = await axios.post('/users', {
+    name,
+    age,
+    married,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+// const data = await response.json();
+  getUsers();
+  // 사용자 목록 조회: 함수 작성
 });
+
+/**
+ * 사용자 목록 조회
+ * GET /users
+ */
+const getUsers = async () => {
+    const res = await axios.get("/users");
+    console.log(res.data);
+};
