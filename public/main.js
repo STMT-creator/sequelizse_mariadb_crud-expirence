be89a69e01175ae3fd8d243922484fcbcb8b044d
STMT <elahep113@naver.com>
2024-12-23 17:08:41 +0900

    getUsers | tbody innerHTML 반복작성

--- public/main.js
+++ public/main.js
@@ -19,7 +19,7 @@ regiForm.addEventListener('submit', async (e) => {
       'Content-Type': 'application/json',
     },
   });
-// const data = await response.json();
+  // const data = await response.json();
   getUsers();
   // 사용자 목록 조회: 함수 작성
 });
@@ -29,6 +29,25 @@ regiForm.addEventListener('submit', async (e) => {
  * GET /users
  */
 const getUsers = async () => {
-    const res = await axios.get("/users");
-    console.log(res.data);
+  const res = await axios.get('/users');
+  const users = res.data;
+
+  const tbody = document.querySelector('#user-list tbody');
+  tbody.innerHTML = '';
+  users.map((user) => {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.textContent = user.id;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = user.name;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = user.age;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = user.married ? '기혼' : '미혼';
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+  });
 };
